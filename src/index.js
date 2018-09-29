8c770a7f998dfd03239ca90f7f843eb65ff8448e
GoldenKit <nikitoska01@gmail.com>
2018-09-29 22:52:38 +0300

    solution additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+var open = [];
+var close = [];
+var buffer = [];
+for (var i = 0; i < str.length; i++) {
+    buffer.push(str[i]);
+  }
+if (buffer.length % 2 !== 0) {
+      return false;
+  }
+for (var i = 0; i < bracketsConfig.length; i++) {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+  }
+for (var i = buffer.length - 1; i >= 0; i--) {
+    for (var j = 0, length = open.length; j < length; j++) {
+        if (buffer[i] === open[j] && buffer[i + 1] === close[j]) {
+           buffer.splice(i, 2);
+            }
+        }
+    }
+    return (buffer.length === 0);
+};
